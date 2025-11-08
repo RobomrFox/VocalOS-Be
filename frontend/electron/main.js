747bd02a3534194e05add1b9a71c83688b95dc97
Johnsinoy <jcsinoy91@gmail.com>
2025-11-08 09:52:38 -0700

    frontend

---
+++ frontend/electron/main.js
@@ -0,0 +1,31 @@
+// electron/main.js
+import { app, BrowserWindow } from "electron";
+import path from "node:path";
+
+const isDev = process.env.NODE_ENV === "development";
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1000,
+    height: 700,
+    frame: true, // we'll do glass mode later
+    transparent: false,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true
+    }
+  });
+
+  if (isDev) {
+    win.loadURL("http://localhost:5173/");
+    win.webContents.openDevTools();
+  } else {
+    win.loadFile(path.join(__dirname, "../dist/index.html"));
+  }
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
